c1decb7202b02e87fd48c48647a8ab30f29a8f2b
monika-hrda <mon.hrda@gmail.com>
2022-02-28 01:08:30 +0000

    Implement logic for selecting game difficulty and switching to a board view, add game board to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-// console.log('Hello');
+const menu = document.querySelector('#menu');
+const difficultyButtons = document.querySelectorAll('.difficulty-button');
+const playButton = document.querySelector('#play-button');
+const game = document.querySelector('#game');
+const board = document.querySelector('#board');
+
+let numOfCards = 6;
+let cards = [];
+
+// Event Listeners
+
+difficultyButtons.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+        const btn = event.target;
+        difficultyButtons.forEach(function (choice) {
+            if (choice.classList.contains('button-active')) {
+                choice.classList.remove('button-active');
+            }
+        });
+        btn.classList.add('button-active');
+        numOfCards = Number(btn.dataset.value);
+        //console.log(numOfCards);
+    })
+});
+
+playButton.addEventListener('click', function () {
+    cards = animalCards.slice(0, numOfCards);
+    menu.style.display = 'none';
+    game.style.display = 'block';
+});
+
+//console.log(board);
